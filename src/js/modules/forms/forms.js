dbfa55138d119d6562b01b23b9c208103329f516
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-05-12 12:49:05 +0300

    forms phoneMask refactoring

--- src/js/modules/forms/forms.js
+++ src/js/modules/forms/forms.js
@@ -1,4 +1,5 @@
 import CheckMailInputs from '../libs/checkMailInputs';
+import PhoneMask from '../libs/phoneMask';
 
 export default class Forms {
 	constructor(forms, url) {
@@ -16,54 +17,6 @@ export default class Forms {
 		this.inputs.forEach((input) => (input.value = ''));
 	}
 
-	phoneMask() {
-		let setCursorPosition = (pos, elem) => {
-			elem.focus();
-
-			if (elem.setSelectionRange) {
-				elem.setSelectionRange(pos, pos);
-			} else if (elem.createTextRange) {
-				let range = elem.createTextRange();
-
-				range.collapse(true);
-				range.moveEnd('character', pos);
-				range.moveStart('character', pos);
-				range.select();
-			}
-		};
-
-		function createMask(event) {
-			let matrix = '+1 (___) ___-____',
-				i = 0,
-				def = matrix.replace(/\D/g, ''),
-				val = this.value.replace(/\D/g, '');
-
-			if (def.length >= val.length) {
-				val = def;
-			}
-
-			this.value = matrix.replace(/./g, function (a) {
-				return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-			});
-
-			if (event.type === 'blur') {
-				if (this.value.length == 2) {
-					this.value = '';
-				}
-			} else {
-				setCursorPosition(this.value.length, this);
-			}
-		}
-
-		let inputs = document.querySelectorAll('[name="phone"]');
-
-		inputs.forEach((input) => {
-			input.addEventListener('input', createMask);
-			input.addEventListener('focus', createMask);
-			input.addEventListener('blur', createMask);
-		});
-	}
-
 	async postData(url, data = {}) {
 		let response = await fetch(url, {
 			method: 'POST',
@@ -74,8 +27,7 @@ export default class Forms {
 
 	init() {
 		new CheckMailInputs().init();
-
-		this.phoneMask();
+		new PhoneMask().init();
 
 		this.forms.forEach((form) => {
 			form.addEventListener('submit', (e) => {
